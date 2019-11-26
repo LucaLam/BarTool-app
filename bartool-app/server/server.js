c5790ef1bbea1ee548bb8959cf67915b9bde2ffd
Luca Lamanna <luca.lamanna@hotmail.com>
2019-11-26 15:09:40 -0500

    initial commit

---
+++ bartool-app/server/server.js
@@ -0,0 +1,15 @@
+//imports
+const express = require('express');
+// const router = require('./router');
+const cors = require('cors');
+
+//use express
+const app = express();
+
+//middleware
+app.use(cors());
+// app.use('/, router);
+
+app.listen(8080, ()=>{
+    console.log('**SERVER READY**');
+})
