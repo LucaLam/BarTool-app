38848d1737db245f138f12e2eee6e3d3c4d24ee7
Luca Lamanna <luca.lamanna@hotmail.com>
2019-12-02 16:21:18 -0500

    Saved Drinks functionality

--- bartool-app/client/src/components/SavedPage.js
+++ bartool-app/client/src/components/SavedPage.js
@@ -10,16 +10,38 @@ export class SavedPage extends Component {
 
     componentDidMount(){
         axios.get("http://localhost:8080/user").then(response => {
-            console.log(response.data)
+            let saved = response.data.filter(user => user.name === localStorage.getItem('user'));
+            saved.map(item => {
+                this.setState({
+                    saved: item.savedDrinks
+                })
+                
+            })
         }
         )}
-
     render() {
-        
-    
+    let savedDrinkList = this.state.saved.map(item => {
+            return(
+                <>
+                <h2>{item.name}</h2>
+                <ul>
+                {
+                    item.ingredients.map(item => {
+                        return(
+                            <li>{item.amount} {item.unit} {item.ingredient}</li>
+                        )
+                    })
+                }
+                </ul>
+                <p>{item.preparation}</p>
+                <p>{item.garnish}</p>
+                </>
+            )
+        })
         return (
             <div>
                 <Header />
+                {savedDrinkList}
                 <Logo />
             </div>
         )
